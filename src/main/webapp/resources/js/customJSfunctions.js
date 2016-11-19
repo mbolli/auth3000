f567ab51bf6c19ee78c5578b6f9f7a51dfb3671b
Dona <galld2@bfh.ch>
2016-11-19 21:23:28 +0100

    test run javascript function

--- src/main/webapp/resources/js/customJSfunctions.js
+++ src/main/webapp/resources/js/customJSfunctions.js
@@ -3,8 +3,7 @@ function generateRandomPassword()
     var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    for( var i=0; i &lt; 16; i++ )
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    for( var i=0; i < 16; i++ ) text += possible.charAt(Math.floor(Math.random() * possible.length));
     alert(text);
     return text;
 }
