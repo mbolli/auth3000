9582d619a0079c46ab873641d4d2fca6e166ebb4
Dona <galld2@bfh.ch>
2016-11-19 21:58:37 +0100

    test run javascript function

--- src/main/webapp/resources/js/customJSfunctions.js
+++ src/main/webapp/resources/js/customJSfunctions.js
@@ -4,7 +4,6 @@ function generateRandomPassword()
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
     for( var i=0; i < 16; i++ ) text += possible.charAt(Math.floor(Math.random() * possible.length));
-    document.getElementById("pass_gen_form:generatedPassLabel").innerHTML = text;
     alert(text);
     return text;
 }
